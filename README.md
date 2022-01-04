f3561094e6d90e874659b4f16bab603aec18b301
Steve Korshakov <steve@korshakov.com>
2022-01-04 23:13:36 +0300

    feat: android support

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # react-native-fast-pbkdf2
 
-PBKDF2 implementation
+PBKDF2 implementation for React Native
+
+* 🔨 Android and iOS native support
+* 🎨 Supports SHA-1, SHA-256, SHA-512
 
 ## Installation
 
@@ -13,14 +16,14 @@ npm install react-native-fast-pbkdf2
 ```js
 import Pbkdf2 from "react-native-fast-pbkdf2";
 
-// ...
+const password = 'cGFzc3dvcmQ=';
+const salt = 'c2FsdA==';
+const numberIterations = 1000;
+const keyLength = 16;
+let res = await Pbkdf2.derive(password, salt, numberIterations, keyLength, 'sha-256');
+// res is Base64 encoded key
 
-const result = await Pbkdf2.multiply(3, 7);
 ```
-
-## Contributing
-
-See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
 
 ## License
 
--- android/src/main/java/com/reactnativepbkdf2/Pbkdf2Module.java
+++ android/src/main/java/com/reactnativepbkdf2/Pbkdf2Module.java
@@ -1,5 +1,7 @@
 package com.reactnativepbkdf2;
 
+import android.util.Base64;
+
 import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.Promise;
@@ -8,27 +10,40 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
 
+import org.spongycastle.crypto.Digest;
+import org.spongycastle.crypto.digests.SHA1Digest;
+import org.spongycastle.crypto.digests.SHA256Digest;
+import org.spongycastle.crypto.digests.SHA512Digest;
+import org.spongycastle.crypto.generators.PKCS5S2ParametersGenerator;
+import org.spongycastle.crypto.params.KeyParameter;
+
 @ReactModule(name = Pbkdf2Module.NAME)
 public class Pbkdf2Module extends ReactContextBaseJavaModule {
-    public static final String NAME = "Pbkdf2";
+  public static final String NAME = "Pbkdf2";
 
-    public Pbkdf2Module(ReactApplicationContext reactContext) {
-        super(reactContext);
+  public Pbkdf2Module(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+  @ReactMethod
+  public void derive(String password, String salt, int iterations, int keySize, String hash, Promise promise) {
+    byte[] decodedPassword = android.util.Base64.decode(password, Base64.DEFAULT);
+    byte[] decodedSalt = android.util.Base64.decode(salt, Base64.DEFAULT);
+    Digest digest = new SHA1Digest();
+    if (hash.equals("sha-256")) {
+      digest = new SHA256Digest();
+    } else if (hash.equals("sha-512")) {
+      digest = new SHA512Digest();
     }
-
-    @Override
-    @NonNull
-    public String getName() {
-        return NAME;
-    }
-
-
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    public void multiply(int a, int b, Promise promise) {
-        promise.resolve(a * b);
-    }
-
-    public static native int nativeMultiply(int a, int b);
+    PKCS5S2ParametersGenerator gen = new PKCS5S2ParametersGenerator(digest);
+    gen.init(decodedPassword, decodedSalt, iterations);
+    byte[] key = ((KeyParameter) gen.generateDerivedParameters(keySize * 8)).getKey();
+    promise.resolve(Base64.encodeToString(key,Base64.DEFAULT));
+  }
 }
